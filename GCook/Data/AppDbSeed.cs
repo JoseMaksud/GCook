1022138ccad1fc03f92e1e40c7727bbe6f37e4a5
José Francisco <125263537+JoseMaksud@users.noreply.github.com>
2024-09-26 21:45:21 -0300

    Atualizando as receitas pois faltava o preparo

--- GCook/Data/AppDbSeed.cs
+++ GCook/Data/AppDbSeed.cs
@@ -123,13 +123,17 @@ public class AppDbSeed
             new Ingrediente() {
                 Id = 13,
                 Nome = "Cheddar"
+            },
+            new Ingrediente() {
+                Id = 14,
+                Nome = "Azeite"
             }
         };
         builder.Entity<Ingrediente>().HasData(ingredientes);
         #endregion
 
         #region Populate Receita
-        List<Receita> receitas = new() {
+         List<Receita> receitas = new() {
             new Receita() {
                 Id = 1,
                 Nome = "Carne Moída Mexicana",
@@ -139,7 +143,13 @@ public class AppDbSeed
                 Rendimento = 3,
                 TempoPreparo = "20 minutos",
                 Foto = "/img/receitas/1.jpg",
-                Preparo = ""
+                Preparo = "Comece pela preparação dos ingredientes, pique os pimentões e a cebola em pequenos cubos, se preferir você também pode usar um processador de alimentos."
+                        + "Coloque a carne moída para fritar em uma panela com um pouco de azeite."
+                        + "Quando a carne moída já não estiver mais crua, adicione os pimentões e a cebola, mexendo bem para misturar todos os ingredientes."
+                        + "Aguarde alguns instante e adicione os temperos, mexendo novamente para misturar."
+                        + "Frite por mais alguns minutos a carne com os demais ingredientes."
+                        + "Adicione o Cream Cheese e o Queijo Cheddar, mexendo bem para evitar que queime o fundo e ajudar os queijos a derreterem."
+                        + "Quando os queijos já estiverem bem derretidos e misturados com os demais ingredientes, sirva acompanhado do Pão Sirio ou de Doritos."
             }
         };
         builder.Entity<Receita>().HasData(receitas);
@@ -206,6 +216,11 @@ public class AppDbSeed
                 ReceitaId = 1,
                 IngredienteId = 13,
                 Quantidade = "200g"
+            },
+            new ReceitaIngrediente() {
+                ReceitaId = 1,
+                IngredienteId = 14,
+                Quantidade = "Um pouco"
             }
         };
         builder.Entity<ReceitaIngrediente>().HasData(receitaIngredientes);
